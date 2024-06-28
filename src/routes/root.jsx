61d8c0ab33d97b5e708aa66c0d697f585947998e
Julian <jjvll90@gmail.com>
2024-06-28 15:59:45 -0500

    Add data loading with React Router: use loader and useLoaderData.

--- src/routes/root.jsx
+++ src/routes/root.jsx
@@ -1,6 +1,10 @@
 import { Outlet } from "react-router-dom";
 import { getContacts } from "../contacts";
 
+export async function loader() {
+    const contacts = await getContacts();
+    return { contacts };
+}
 
 
 export default function Root() {
@@ -34,10 +38,10 @@ export default function Root() {
         <nav>
         <ul>
             <li>
-            <Link to={`/contacts/1`}>Your Name</Link>
+            <Link to={`contacts/1`}>Your Name</Link>
             </li>
             <li>
-            <Link to={`/contacts/2`}>Your Friend</Link>
+            <Link to={`contacts/2`}>Your Friend</Link>
             </li>
         </ul>
         </nav>
